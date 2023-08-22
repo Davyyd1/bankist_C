9d994014d8817120f7be5617e99b7486d22d1d20
Davyyd1 <d.dav889@gmail.com>
2023-08-22 11:16:27 +0300

    chaining using map filter reduce methods

--- challenge.js
+++ challenge.js
@@ -17,17 +17,17 @@
 
 // checkDogs([3, 5, 2, 12, 7],  [4, 1, 15, 8, 3]);
 
-// const calcAverageHumanAge = function (ages) {
-//     const humanAges = ages.map(age => age <= 2 ? 2*age : 16+age*4);
-//     console.log(humanAges);
-//     const adults = humanAges.filter((cur) => cur >= 18)
-//     console.log(humanAges);
-//     console.log(adults);
-//     const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
-//     return average
-// }
+const calcAverageHumanAge2 = function (ages) {
+    const humanAges = ages.map(age => age <= 2 ? 2*age : 16+age*4);
+    const adults = humanAges.filter((cur) => cur >= 18)
+    const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+    return average
+}
 
-// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
-// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
-// console.log(avg1, avg2);
+const calcAverageHumanAge = ages => ages.map(age => age <= 2 ? 2*age : 16+age*4).filter(cur=> cur >= 18).reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
+
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+console.log(avg1, avg2);
 
